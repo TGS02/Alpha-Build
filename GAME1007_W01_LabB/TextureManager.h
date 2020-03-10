0f5f90d8f6569dcc610c1bc4d4387d0a4d6d7b4c
Kiera Bacon <kierajbacon@outlook.com>
2020-03-10 14:01:11 -0400

    Added: Camera Class and SUPER basic functionality for it.

--- GAME1007_W01_LabB/TextureManager.h
+++ GAME1007_W01_LabB/TextureManager.h
@@ -6,7 +6,7 @@ public:
 	static SDL_Texture* LoadTexture(SDL_Renderer* m_pRenderer,const char* fileName);
 	static void Draw(SDL_Renderer* m_pRenderer,SDL_Texture* tex, SDL_Rect* src, SDL_Rect* dst);
 	static void DrawLeft(SDL_Renderer* m_pRenderer, SDL_Texture* tex, SDL_Rect* src, SDL_Rect* dst);
-	static void draw(SDL_Renderer* pRenderer, SDL_Texture* tex, SDL_Rect* src, SDL_Rect* dst, double angle, int alpha, SDL_Point* centered, SDL_RendererFlip flip);
+	static void draw(SDL_Renderer* pRenderer, SDL_Texture* tex, SDL_Rect* src, SDL_Rect* dst, double angle = 0.0, int alpha = 0, SDL_Point* centered = nullptr, SDL_RendererFlip flip = SDL_FLIP_NONE);
 
 };
 
