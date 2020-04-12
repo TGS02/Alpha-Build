8a16aec8a252aed4f27efb14924563ad690e4cd1
Chris Tulip <Tulip.Chris@gmail.com>
2020-04-11 23:13:38 -0400

    Added Level Previews
    also fixed end state on last level to send you back to menu

--- GAME1007_W01_LabB/FSM.h
+++ GAME1007_W01_LabB/FSM.h
@@ -133,13 +133,17 @@ private:
 	SDL_Texture* m_pLogoTex;
 	SDL_Rect m_bgSrc, m_bgDst, m_logoSrc, m_logoDst;
 	vector<Button*> m_vButtons;
+	vector<SDL_Texture*> m_pLevels;
 	
 public:
 	int level_set;
 	LevelSelect(int i);
+	void loadPreviews();
 	void Enter();
 	void Update();
 	void Render();
+	void displayPreviews();
+	void loadButtons();
 	void Exit();
 };
 
